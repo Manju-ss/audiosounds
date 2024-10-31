4c66bc6ba04045fbcc245964bad56f137b7b90ff
Manjunath <manjuanthsachin@gmail.com>
2024-10-31 19:41:53 +0530

    NEW PROJECT

--- src/main/java/com/sound/audiosounds/awss3config/S3Configuration.java
+++ src/main/java/com/sound/audiosounds/awss3config/S3Configuration.java
@@ -5,14 +5,17 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class S3Configuration {
 
-    private String accesskey = "AKIAX5ZVY3ZF6UTTONBU";
-    private String secrateKey = "ldEzmgCJ5AsL+zXNf2AmZlJUhcXGN0ABo+T6sfCi";
+    @Value("${accesskey = AKIAX5ZVY3ZF6UTTONBU}")
+    private static String accesskey ;
+    @Value("${secrateKey = ldEzmgCJ5AsL+zXNf2AmZlJUhcXGN0ABo+T6sfCi}")
+    private static  String secrateKey;
     private String region = "ap-south-1";
 
     @Bean
